9714496e122fcc9472a1be02070100bb1bc67cc0
高俊东 <1643549042@qq.com>
2024-02-27 17:06:09 +0800

    ✨ feat: gzip压缩

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import viteCompression from 'vite-plugin-compression'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    viteCompression({
+      ext: '.gz',
+    })
+  ],
   base: '/vue3',
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
