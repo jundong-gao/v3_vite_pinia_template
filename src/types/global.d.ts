0d6c78f6b684403045273b45f6417126b7e9447d
高俊东 <1643549042@qq.com>
2024-03-20 09:29:34 +0800

    🐞 fix: d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,7 +1,7 @@
 import { CancelToken } from "axios"
 
 /**
- * axios请求默认返回格式
+ * @description axios请求默认返回格式
  * @param err {Number} 返回状态
  * @param data {T} 返回数据
  */
@@ -12,6 +12,9 @@ export interface INormalResponse<T> {
 }
 
 export type INormalRequest<T> = {
+    /**
+     * @description cancelToken
+     */
     cancelToken?: CancelToken
 } & T
 
