d28784b511c8891e388e8b7c708a24ad94e1ab64
高俊东 <1643549042@qq.com>
2024-03-21 15:20:03 +0800

    🗯  chore: 按需区引入

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,7 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import elementUI from 'element-plus'
-import 'element-plus/dist/index.css'
 import '@/styles/tailwind.css'
-// import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-// import globalComps from '@/components/global/index'
 
 
 const app = createApp(App)
@@ -19,11 +15,7 @@ const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
 
-// for (const [key, component] of Object.entries(globalComps)) {
-//     app.component(key, component as ComponentOptions)
-// }
 
-app.use(elementUI)
 app.use(pinia)
 app.use(router)
 
